bf9d70cf2ab977191ace1621f9b88f3efe242d27
CedricMlg <malingre.cedric@gmail.com>
2023-01-06 18:42:14 +0100

    Add style to error page + fix some issues in code

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -1,9 +1,13 @@
+import { Link } from "react-router-dom";
+
 function Error() {
   return (
     <div className="Error">
       <h1>404</h1>
-      <p>Oups! La page que vous demandez n'existe pas.</p>
-      <a href="">Retourner sur la page d'accueil</a>
+      <p>
+        Oups! La page que <br /> vous demandez n'existe pas.
+      </p>
+      <Link to="/">Retourner sur la page d'accueil</Link>
     </div>
   );
 }
