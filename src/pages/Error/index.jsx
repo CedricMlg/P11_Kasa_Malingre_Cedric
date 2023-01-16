d10c4e36b9de4522e49421cf1e6449468ca9b575
CedricMlg <malingre.cedric@gmail.com>
2023-01-16 15:21:45 +0100

    Add style to desktop error page

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -5,7 +5,7 @@ function Error() {
     <div className="error">
       <h1>404</h1>
       <p>
-        Oups! La page que <br /> vous demandez n'existe pas.
+      <span>Oups! La page que</span> <span>vous demandez n'existe pas.</span>
       </p>
       <Link to="/">Retourner sur la page d'accueil</Link>
     </div>
